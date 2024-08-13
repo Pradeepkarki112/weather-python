d1df7f03a79a1f0666fec11a8a7f5a08694277b2
Pradeep Kakri <pradeepkarki112@gmail.com>
2024-08-13 09:14:22 +0545

    page

--- weather.py
+++ weather.py
@@ -1,12 +1,33 @@
-import requests
 from dotenv import load_dotenv
-import os
 from pprint import pprint
+import requests
+import os
 
 load_dotenv()
 
+def get_current_weather(city="Bharatpur"):
 
-def get_current_weather():
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+  
+  
+  
+if __name__ == "__main__":
+    print('\n*** Get Current Weather Conditions ***\n')
+
+    city = input("\nPlease enter a city name: ")
+
+    weather_data = get_current_weather(city)
+
+    print("\n")
+    pprint(weather_data)
+
+
+
+def weather():
     print('\n*** Get Current Weather Conditions ***\n')
 
     city = input("\nEnter city name: \n")
@@ -24,5 +45,5 @@ def get_current_weather():
     print(
           f'\n{weather_data["weather"][0]["description"].capitalize()} and feels like {weather_data["main"]["feels_like"]:.1f}°\n')
     
-if __name__ == "__main__":
-    get_current_weather()
+# if __name__ == "__main__":
+#     weather()
